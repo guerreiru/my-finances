fb136f9085e587243c3010925ef8e98be82c3bec
Fernando Guerreiro <fernando.rodrigues@prodap.com.br>
2025-05-16 18:52:48 -0300

    fix: remove favico from manifest

--- src/app/manifest.ts
+++ src/app/manifest.ts
@@ -12,11 +12,6 @@ export default function manifest(): MetadataRoute.Manifest {
     orientation: "portrait",
     icons: [
       {
-        src: "/favicon.ico",
-        sizes: "any",
-        type: "image/x-icon",
-      },
-      {
         src: "/icons/icon-192x192.png",
         sizes: "192x192",
         type: "image/png",
